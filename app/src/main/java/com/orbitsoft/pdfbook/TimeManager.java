55c49cc1d6c21e4629f7384bcdf82b673bed1a14
rmsod <rmsod9803@gail.com>
2022-12-28 20:12:06 +0330

    test

---
+++ app/src/main/java/com/orbitsoft/pdfbook/TimeManager.java
@@ -0,0 +1,14 @@
+package com.orbitsoft.pdfbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_manager);
+    }
+}
