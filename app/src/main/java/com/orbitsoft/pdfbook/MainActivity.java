204d15dce2817f25c49be18911801a9c4f3f334e
rmsod <rmsod9803@gail.com>
2022-12-31 00:38:09 +0330

    BookAlarm

--- app/src/main/java/com/orbitsoft/pdfbook/MainActivity.java
+++ app/src/main/java/com/orbitsoft/pdfbook/MainActivity.java
@@ -1,14 +1,117 @@
 package com.orbitsoft.pdfbook;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.Calendar;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    TimePicker tp_time;
+    TextView tv_display;
+    Button btn_set, btn_reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_display = (TextView)findViewById(R.id.tv_display);
+        tp_time = (TimePicker)findViewById(R.id.tp_time);
+        btn_set = (Button)findViewById(R.id.btn_set);
+        btn_reset = (Button)findViewById(R.id.btn_reset);
+
+        btn_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+
+                if(Build.VERSION.SDK_INT >= 23) {
+
+                    calendar.set(
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            tp_time.getHour(),
+                            tp_time.getMinute(),
+                            0
+                    );
+
+
+
+                }else{
+                    calendar.set(
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            tp_time.getCurrentHour(),
+                            tp_time.getCurrentMinute(),
+                            0
+                    );
+                }
+
+
+                setAlarm(calendar.getTimeInMillis(), calendar);
+            }
+
+
+            private void setAlarm(long timeInMillis, Calendar c) {
+                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                Intent intent = new Intent(MainActivity.this, AlarmAdapter.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+                alarmManager.setRepeating(AlarmManager.RTC, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+
+                Toast.makeText(MainActivity.this, "تنظیم زمان", Toast.LENGTH_SHORT).show();
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+                int ampm = c.get(Calendar.AM_PM);
+                String day = "";
+                if(ampm == Calendar.AM){
+                    day = "AM";
+                }else if(ampm == Calendar.PM){
+                    day = "PM";
+                }
+                String timeText = "زمان مطالعه بعدی: ";
+                timeText += minute +": " +  hour+ " " + day;
+                tv_display.setText(timeText);
+
+            }
+
+
+        });
+
+
+
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+                Intent intent = new Intent(MainActivity.this, AlarmAdapter.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+                alarmManager.cancel(pendingIntent);
+
+                tv_display.setText("زمان مطالعه تنظیم نشده");
+
+            }
+        });
     }
 }
