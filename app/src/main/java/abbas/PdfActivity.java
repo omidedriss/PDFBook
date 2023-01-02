e2cb60a944fb01eb9d08250320afaf6e297e9382
Developer <absgorji@gmail.com>
2023-01-02 15:54:08 +0330

    اضافه کردن اکتیویتی جدید برای اجرای پی دی اف در آن اکتیویتی ولی تست گرفته نشده به دلیل ارری ک برنامه داره و هنوز کار داره اضافه کردن دکمه دایره شکل به صفحه اصلی (در ادامه اضافه کردن نویگیشن ویو به برنامه هستش ) اضافه کردن آیکون به برنامه

---
+++ app/src/main/java/abbas/PdfActivity.java
@@ -0,0 +1,16 @@
+package abbas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.orbitsoft.pdfbook.R;
+
+public class PdfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+    }
+}
--- app/src/main/java/com/orbitsoft/pdfbook/MainActivity.java
+++ app/src/main/java/com/orbitsoft/pdfbook/MainActivity.java
@@ -4,7 +4,10 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
@@ -16,13 +19,25 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 
-public class MainActivity extends AppCompatActivity {
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+
+import java.sql.Time;
+
+//import top.defaults.colorpicker.ColorPickerPopup;
+//import com.xeoh.android.texthighlighter.TextHighlighter;
+
+public class MainActivity extends AppCompatActivity implements OnDrawListener {
+    private Button button,time;
 
     // Initialize variable
+    private int selectedColor=0;
     Button btSelect;
     TextView tvUri, tvPath;
     ActivityResultLauncher<Intent> resultLauncher;
@@ -32,12 +47,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.select);
+
+        setContentView(R.layout.add_btn);
+
+        button=(Button) findViewById(R.id.bt_add);
+       /* time=(Button) findViewById(R.id.time1);
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, TimeActivity.class);
+                startActivity(i);
+            }
+        });*/
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+
+
+                //myNotification  myotification=new myNotification(MainActivity.this);
+                // myotification.showNotification();
+
+                //getMyColor(v);
+
+
+
+
+            }
+        });
+
+
+
+
 
         // assign variable
         btSelect = findViewById(R.id.bt_select);
         tvUri = findViewById(R.id.tv_uri);
         tvPath = findViewById(R.id.tv_path);
+        PDFView pdfView = findViewById(R.id.pdfView);
 
         // Initialize result launcher
         resultLauncher = registerForActivityResult(
@@ -55,17 +103,55 @@ public class MainActivity extends AppCompatActivity {
                             // When data is not equal to empty
                             // Get PDf uri
                             Uri sUri = data.getData();
-                            // set Uri on text view
-                            tvUri.setText(Html.fromHtml(
-                                    "<big><b>PDF Uri</b></big><br>"
-                                            + sUri));
-
                             // Get PDF path
                             String sPath = sUri.getPath();
                             // Set path on text view
                             tvPath.setText(Html.fromHtml(
                                     "<big><b>PDF Path</b></big><br>"
                                             + sPath));
+                            // set Uri on text view
+                            pdfView.fromUri(sUri).enableSwipe(true) // allows to block changing pages using swipe
+                                    .swipeHorizontal(false)
+                                    .enableDoubletap(true)
+                                    .defaultPage(0)
+                                    // allows to draw something on the current page, usually visible in the middle of the screen
+                                    .onDraw(MainActivity.this)
+                                    // allows to draw something on all pages, separately for every page. Called only for visible pages
+                                    // .onDrawAll(onDrawListener)
+                                    // .onLoad(onLoadCompleteListener) // called after document is loaded and starts to be rendered
+                                    // .onPageChange(onPageChangeListener)
+                                    // .onPageScroll(onPageScrollListener)
+                                    // .onError(onErrorListener)
+                                    // .onPageError(onPageErrorListener)
+                                    // .onRender(onRenderListener) // called after document is rendered for the first time
+                                    // called on single tap, return true if handled, false to toggle scroll handle visibility
+                                    //  .onTap(onTapListener)
+                                    //  .onLongPress(onLongPressListener)
+                                    .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                                    .password(null)
+                                    .scrollHandle(null)
+                                    .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                                    // spacing between pages in dp. To define spacing color, set view background
+                                    .spacing(0)
+                                    .autoSpacing(false) // add dynamic spacing to fit each page on its own on the screen
+                                    //   .linkHandler(DefaultLinkHandler)
+                                    .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                                    .fitEachPage(false) // fit each page to the view, else smaller pages are scaled relative to largest page.
+                                    .pageSnap(false) // snap pages to screen boundaries
+                                    .pageFling(false) // make a fling change only a single page like ViewPager
+                                    .nightMode(false) // toggle night mode
+                                    .load();
+//                            new TextHighlighter()
+//                                    .setBackgroundColor( Color.parseColor( "#FFFF00" ) )
+//                                    .setForegroundColor( Color.GREEN )
+//                                    .addTarget( tvUri )
+//                                    .highlight( tvUri.getText().toString(), TextHighlighter.BASE_MATCHER );
+
+//                            tvUri.setText(Html.fromHtml(
+//                                    "<big><b>PDF Uri</b></big><br>"
+//                                            + sUri));
+
+
                         }
                     }
                 });
@@ -99,6 +185,24 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+   /* private void getMyColor(View v) {
+        new ColorPickerPopup.Builder(this)
+                .initialColor(Color.BLUE)
+                .enableAlpha(true)
+                .okTitle("Choose")
+                .cancelTitle("Cancel")
+                .showIndicator(true)
+                .showValue(true)
+                .onlyUpdateOnTouchEventUp(true)
+                .build()
+                .show(v, new ColorPickerPopup.ColorPickerObserver() {
+                    @Override
+                    public void onColorPicked(int color) {
+                        selectedColor=color;
+                    }
+                });
+    }*/
 
     private void selectPDF()
     {
@@ -137,4 +241,23 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         }
     }
+
+    @Override
+    public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+
+        canvas.drawColor(selectedColor);
+
+        // canvas.drawColor();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
